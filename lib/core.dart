b976621e080fd5b65d002f178ccfae54773cf31f
Jairo Fernandez <jairofg12@gmail.com>
2020-12-12 23:03:03 -0500

    initial

---
+++ lib/core.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library core;
+
+export 'src/core_base.dart';
+
+export 'package:console/console.dart';
+export 'package:dot_env/dot_env.dart';
